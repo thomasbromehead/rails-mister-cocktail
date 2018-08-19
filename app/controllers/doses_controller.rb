906e1485620ed733e32df30a3e1c54100da07a60
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-08-19 09:11:55 +0200

    Resetting doc after keychain init

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -42,7 +42,7 @@ class DosesController < ApplicationController
   private
   
   def find_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id]ss)
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def find_ingredient
